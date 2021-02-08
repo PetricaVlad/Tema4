e39bd3f6eef8359de1b0f66d13fb15123e595aae
Petrica Vlad <petricavlad16@yahoo.ro>
2021-02-08 19:26:18 +0200

    Update PreventRequestsDuringMaintenance.php

--- app/Http/Middleware/PreventRequestsDuringMaintenance.php
+++ app/Http/Middleware/PreventRequestsDuringMaintenance.php
@@ -3,5 +3,5 @@ namespace App\Http\Middleware;
 use Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance as Middleware;
 class PreventRequestsDuringMaintenance extends Middleware
 {
-    protected $except=[];
+protected $except=[];
 }
